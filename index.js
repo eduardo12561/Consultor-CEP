329b701bcd186f4a4a6efbf1641b7010d0ecb1d8
Eduardo Breda <pontes.cisa@gmail.com>
2022-01-31 14:19:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('ddd').value = endereco.ddd;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+    
+}
+
+const pesquisarCep = async() => {
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    
+    const dados = await fetch(url)
+    const endereco = await dados.json();
+    preencherFormulario(endereco)
+    
+}
+
+document.getElementById('cep')
+    .addEventListener('focusout', pesquisarCep);
+
